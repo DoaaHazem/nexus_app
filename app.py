4bc2eb08274860a64ac11d4dc9b3011ee0d59e89
DoaaHazem <doaahazem711@gmail.com>
2025-02-16 23:34:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def cheating():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
